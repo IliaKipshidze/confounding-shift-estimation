8fb82ae45fba0264f6e2f192af4a732f2cbfe98f
Ilia Kipshidze <iliakipshidze1@gmail.com>
2026-03-20 01:58:23 +0100

    added data from remote deskt

--- TabZilla/run_hard.sh
+++ TabZilla/run_hard.sh
@@ -1,14 +1,40 @@
 #!/usr/bin/env bash
 
-source ../scripts/HARD_DATASETS_BENCHMARK.sh
+DATASETS=(
+  "openml__albert__189356"
+  "openml__artificial-characters__14964"
+  "openml__audiology__7"
+  "openml__balance-scale__11"
+  "openml__cnae-9__9981"
+  "openml__colic__25"
+  "openml__credit-approval__29"
+  "openml__credit-g__31"
+  "openml__electricity__219"
+  "openml__elevators__3711"
+  "openml__guillermo__168337"
+  "openml__heart-h__50"
+  "openml__higgs__146606"
+  "openml__jasmine__168911"
+  "openml__jungle_chess_2pcs_raw_endgame_complete__167119"
+  "openml__kc1__3917"
+  "openml__lymph__10"
+  "openml__mfeat-fourier__14"
+  "openml__mfeat-zernike__22"
+  "openml__monks-problems-2__146065"
+  "openml__nomao__9977"
+  "openml__one-hundred-plants-texture__9956"
+  "openml__phoneme__9952"
+  "openml__poker-hand__9890"
+)
 
-models=("XGBoost" "CatBoost" "TabNet" "TabTransformer" "TabPFNModel" "rtdl_ResNet")
+models=("LightGBM")
 base_config="./tabzilla_experiment_config_gpu.yml"
 python_bin="/home/ilia.kipshidze/projects/confounding-shift-estimation/Tabz/bin/python"
-run_name="hard_run_$(date +%Y%m%d_%H%M%S)"
+run_name="hard_run_LightGBM_batch_of_24"
 failures=0
 
-for ds in "${DATASETS[@]}"; do
+for ((i=${#DATASETS[@]}-1; i>=0; i--)); do
+  ds="${DATASETS[i]}"
   dataset_dir="./datasets/$ds"
 
   if [ ! -d "$dataset_dir" ]; then
